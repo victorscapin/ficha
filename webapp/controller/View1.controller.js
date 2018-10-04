ff04b49617b0c15aa823244e0c3bbfe4ccbdf014
dos Santos Carlos Fernando <carlos.santos@forlogic.net>
2018-10-04 12:08:47 +0000

    Criação do crud de Tratamento Fita

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -18,6 +18,9 @@ sap.ui.define([
 		},
 		onToRouteSondagem: function() {
 			this.getOwnerComponent().getRouter().navTo("RouteSondagem");
+		},
+		onToRouteTratamentoFita: function() {
+			this.getOwnerComponent().getRouter().navTo("RouteTratamentoFita");
 		}
 		
 	});
